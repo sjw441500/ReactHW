d9e726f9a7ce48bcfe140490c724f9371877fe3a
khu <sjw441500@gmail.com>
2019-02-09 10:08:07 -0800

    HW6 folks haochuan

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import HW4 from './HW4';
+import HW6 from './HW6';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <HW4 />
+      <HW6 />
 
       </div>
     );
